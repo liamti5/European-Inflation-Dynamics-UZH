39a99ec3f308653966d04be91631d46d60814e30
Liam <liam.tessendorf@uzh.ch>
2024-11-26 17:39:34 +0100

    dataset.py - formatting

--- european_inflation_dynamics/dataset.py
+++ european_inflation_dynamics/dataset.py
@@ -9,6 +9,7 @@ from config import PROCESSED_DATA_DIR, RAW_DATA_DIR, EXTERNAL_DATA_DIR
 
 app = typer.Typer()
 
+
 def data_inflation_rates():
     df = pd.read_csv(EXTERNAL_DATA_DIR / "oecd-european-inflation-rates-since-2000.csv")
     df = df[["Reference area", "TIME_PERIOD", "OBS_VALUE"]]
@@ -21,7 +22,9 @@ def data_inflation_rates():
 
 
 def data_long_lat():
-    df_european_countries = pd.read_feather(PROCESSED_DATA_DIR / "countries-with-inflation-data.ftr").rename(columns={"Reference area": "name"})
+    df_european_countries = pd.read_feather(
+        PROCESSED_DATA_DIR / "countries-with-inflation-data.ftr"
+    ).rename(columns={"Reference area": "name"})
     country_name_changes = {
         "Turkey": "Türkiye",
         "Czech Republic": "Czechia",
@@ -33,16 +36,20 @@ def data_long_lat():
     df_long_lat = pd.merge(df_european_countries, df_all_countries, on="name", how="inner")
     df_long_lat.to_feather(PROCESSED_DATA_DIR / "countries-with-long-lat-data.ftr")
 
+
 @app.command()
 def main():
     logger.info("Processing dataset...")
 
-    data_functions = [(name, func) for name, func in globals().items() if callable(func) and name.startswith('data_')]
-    for (_, function) in tqdm(data_functions, total=len(data_functions)):
+    data_functions = [
+        (name, func)
+        for name, func in globals().items()
+        if callable(func) and name.startswith("data_")
+    ]
+    for _, function in tqdm(data_functions, total=len(data_functions)):
         function()
 
     logger.success("Processing datasets complete.")
-
 
 
 if __name__ == "__main__":
